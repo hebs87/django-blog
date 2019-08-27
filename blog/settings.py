2936fedbfa0475845a98b82393f690587c61171b
Ubuntu <ubuntu@ip-172-31-89-188.ec2.internal>
2019-08-27 09:10:21 +0000

    Installed django-forms-bootstrap and configured settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-# -----------------------------EDITED-----------------------------
+    # -----------------------------EDITED-----------------------------
+    'django_forms_bootstrap',
     'posts',
 ]
 
